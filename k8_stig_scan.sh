7ad452df1f7af9549991e136947ca68091713f78
Avinash <savaliya.avi@gmail.com>
2022-06-08 13:36:54 +0530

    Create k8_stig_scan.sh

---
+++ k8_stig_scan.sh
@@ -0,0 +1,12 @@
+cd alltkc
+wget https://raw.githubusercontent.com/yogeshbendre/specialtools/master/k8_stig.yaml -O k8-stig.yaml
+kclist=$(ls | grep kubeconfig | head -n 5)
+
+for i in $kclist
+do
+echo $i
+kubectl --kubeconfig $i delete ns k8-stig || true
+kubectl --kubeconfig $i create ns k8-stig || true
+kubectl --kubeconfig $i -n k8-stig apply -f k8-stig.yaml || true
+done
+sleep 30
