27909e8b23b7136f267af4154c95506c56a847cf
Avinash <savaliya.avi@gmail.com>
2022-06-09 00:02:12 +0530

    Update k8_stig_scan.sh

--- k8_stig_scan.sh
+++ k8_stig_scan.sh
@@ -1,13 +1,15 @@
 cd alltkc
 wget https://github.com/avinashsavaliya/borathon/blob/main/k8_stig_v1.yaml -O k8_stig_v1.yaml
-kclist=$(ls | grep kubeconfig | head -n 2)
-sshkeylist=$(ls | grep ssh | head -n 2)
+# kclist=$(ls | grep kubeconfig | head -n 2)
+# sshkeylist=$(ls | grep ssh | head -n 2)
+sshkeylist=("tkg216-antrea-35ns5-c5-ssh")
+kclist=("tkg216-antrea-35ns5-c5-kubeconfig")
 for i in $kclist
 do
 echo $i
 kubectl --kubeconfig $i delete ns k8-stig || true
 kubectl --kubeconfig $i create ns k8-stig || true
-kubectl --kubeconfig $i -n k8-stig apply -f k8-stig.yaml || true
+kubectl --kubeconfig $i -n k8-stig apply -f k8_stig_v1.yaml || true
 done
 sleep 30
 for i in $kclist
@@ -20,7 +22,7 @@ b=($a) || true
 podname=${b[1]} || true
 # copy ssh file from host to pod
 kubectl --kubeconfig $i cp $sshfile $podname:/share/ || true
-#retrive controlplane and worker node ip
+#retrieve controlplane and worker node ip
 ips=$(kubectl --kubeconfig $i get nodes -o json | jq '.items[].status.addresses[] | select(.type=="InternalIP") | .address'|| true)
 op=($ips) || true
 cpip=${op[0]} || true
@@ -30,6 +32,8 @@ echo $workerip
 cpresult=${kc/kubeconfig/"cp.text"}
 workerresult=${kc/kubeconfig/"worker.text"}
 # run script on pod for cp node
+echo "kubectl --kubeconfig $i exec $podname -i -t  -- /share/stig_scanner.sh $cpip $sshfile > alltkc/$cpresult"
 kubectl --kubeconfig $i exec $podname -i -t  -- /share/stig_scanner.sh $cpip $sshfile > alltkc/$cpresult
+sleep 10
 done
-sleep 30
+
