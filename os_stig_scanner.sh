de585a0840ea09218c13ad695a3124800818afc7
Avinash <savaliya.avi@gmail.com>
2022-06-09 12:30:53 +0530

    Update os_stig_scanner.sh

--- os_stig_scanner.sh
+++ os_stig_scanner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /share/kubernetes-stig-baseline
+cd /share/photon-3-stig-inspec-baseline
 echo "inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/output.json"
 inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/output.json 
