194ce94b9504443679f574f133083d5476358c3a
Avinash <savaliya.avi@gmail.com>
2022-06-14 11:27:37 +0530

    Update stig_scanner_worker.sh

--- stig_scanner_worker.sh
+++ stig_scanner_worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /share/kubernetes-stig-baseline
-echo "inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/output.json --controls=V-242387 V-242391 V-242392 V-242393 \
+echo "inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/$3 --controls=V-242387 V-242391 V-242392 V-242393 \
     V-242394 V-242396 V-242397 V-242398 V-242399 \
     V-242400 V-242404 V-242406 V-242407 V-245541 \
     V-242420 V-242425 V-242434 V-242449 V-242450 \
