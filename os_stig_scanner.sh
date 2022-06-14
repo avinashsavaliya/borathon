ee624ad33b2e53da51860f3f18695ff735da10f5
Avinash <savaliya.avi@gmail.com>
2022-06-14 11:41:00 +0530

    Update os_stig_scanner.sh

--- os_stig_scanner.sh
+++ os_stig_scanner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /share/photon-3-stig-inspec-baseline
-echo "inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/output.json"
-inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/output.json 
+echo "inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/$3"
+inspec exec . --chef-license accept -t ssh://vmware-system-user@$1 -i /share/$2 --sudo --reporter=cli json:/share/$3
